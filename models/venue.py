c397863aef0d67d3c8b4d694f58f5718e7001fae
franc <onyishif@gmail.com>
2022-06-01 21:15:34 +0100

    change genre to genres

--- models/venue.py
+++ models/venue.py
@@ -12,7 +12,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean())
     seeking_description = db.Column(db.String, nullable=True)
-    genre = db.Column(db.String)
+    genres = db.Column(db.String)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     event = db.relationship('Show', backref='venue', cascade="all, delete")
