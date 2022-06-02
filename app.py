32a6c075825d60b66105cd18bf2e37bf7e7fdc3f
franc <onyishif@gmail.com>
2022-06-02 13:57:42 +0100

    edit artist

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 #----------------------------------------------------------------------------#
 
 import json
+from re import A
 import sys
 from unicodedata import name
 from wsgiref.handlers import format_date_time
@@ -271,7 +272,7 @@ def show_artist(artist_id):
   # shows the artist page with the given artist_id
   # TODO: replace with real artist data from the artist table, using artist_id
   data1={
-    "id": 4,
+    "id": 1,
     "name": "Guns N Petals",
     "genres": ["Rock n Roll"],
     "city": "San Francisco",
@@ -348,28 +349,30 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-  form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
-  # TODO: populate form with fields from artist with ID <artist_id>
+  artist = Artist.query.get(artist_id)
+  if not artist:
+    flash('Artist with id '+ str(artist_id) + ' does not exist')
+    return render_template('pages/home.html')
+  dictObj = lambda r: {c.name: str(getattr(r, c.name)) for c in r.__table__.columns}
+  # get the dictionary rep of the venue obj
+  artistDict = dictObj(artist)
+  #set the genres back to array
+  artistDict['genres'] = artistDict["genres"].split(',')
+  form = ArtistForm(data = artistDict)
+
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  # TODO: take values from the form submitted, and update existing
   # artist record with ID <artist_id> using the new attributes
-
+  form = ArtistForm()
+  if form.validate_on_submit():
+    genreString = ','.join(str(x) for x in form.data['genres'])
+    artist = Artist.query.get(artist_id)
+    form.populate_obj(artist)
+    # set the genre back to string
+    artist.genres = genreString
+    db.session.commit()
   return redirect(url_for('show_artist', artist_id=artist_id))
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
