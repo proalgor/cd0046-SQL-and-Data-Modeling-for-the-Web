3208be04e558cdaf261fb06a937361cf05ba70bd
franc <onyishif@gmail.com>
2022-06-02 13:33:02 +0100

    edit venues

--- app.py
+++ app.py
@@ -374,28 +374,27 @@ def edit_artist_submission(artist_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
+  venue = Venue.query.get(venue_id)
+  dictObj = lambda r: {c.name: str(getattr(r, c.name)) for c in r.__table__.columns}
+  # get the dictionary rep of the venue obj
+  venueDictionary = dictObj(venue)
+  #set the genres back to array
+  venueDictionary['genres'] = venueDictionary["genres"].split(',')
+  form = VenueForm(data = venueDictionary)
+  
   return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-  # TODO: take values from the form submitted, and update existing
   # venue record with ID <venue_id> using the new attributes
+  form = VenueForm()
+  if form.validate_on_submit():
+    genreString = ','.join(str(x) for x in form.data['genres'])
+    venue = Venue.query.get(venue_id)
+    form.populate_obj(venue)
+    # set the genre back to string
+    venue.genres = genreString
+    db.session.commit()
   return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Create Artist
